32aa2c197900690ddc203a9a6feefa662ffa2be9
Ian Serlin <ian@ianserlin.com>
2014-04-30 16:02:00 -0400

    wee data bits

--- meteor-cloud-keepalive.js
+++ meteor-cloud-keepalive.js
@@ -1,6 +1,8 @@
 Logs = new Meteor.Collection('logs');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('logs');
+
   Template.SiteList.helpers({
     sites: function () {
       return Meteor.settings.public.sites;
@@ -9,21 +11,38 @@ if (Meteor.isClient) {
 
   Template.Log.helpers({
     logs: function () {
-      return Logs.find({}, { sort: {createdAt: -1}, limit: 10 * Meteor.settings.public.sites.length });
+      return Logs.find({}, { sort: {timestamp: -1}, limit: 10 * Meteor.settings.public.sites.length });
+    }
+    , status: function(log){
+      return log.success === true ? "UP" : "DOWN";
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.setTimeout(function(){
+  Meteor.publish('logs', function(){
+    return Logs.find({}, { sort: {timestamp: -1}, limit: 10 * Meteor.settings.public.sites.length })
+  });
+
+  function ping(){
     var sites = Meteor.settings.public.sites;
     _.each(sites, function(site){
-      var result = HTTP.get(site.url);
-      Logs.insert({
-        site: site
-        , timestamp: new Date()
-      })
+      HTTP.get(site.url, function(err, result){
+        Logs.insert({
+          site: site
+          , timestamp: new Date()
+          , error: result.statusCode
+          , success: !err
+        });
+      });
     });
-    HTTP.get()
+  }
+
+  Meteor.setTimeout(function(){
+    ping();
   }, 1000 * 60 * 30); // 30 minute pings
+
+  Meteor.startup(function(){
+    ping();
+  });
 }
